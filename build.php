b2448a029d4ada1e2b7856e87b1b0cfd129e3d85
Chris Chasm <chris@chasm.solutions>
2019-08-14 13:54:33 -0600

    Hierarchy Build files

--- build.php
+++ build.php
@@ -261,6 +261,107 @@ print '#';
 /** END CREATE CENSUS FILE */
 
 
+
+
+
+/*******************************************************************************************************
+ *
+ * Build Hierarchy Files
+ *
+ *******************************************************************************************************/
+print '#';
+// build list array
+$grid_id = '';
+$response = [];
+// pre-start list
+$start_grid_id = 1;
+$parent_object = mysqli_query( $con, "
+        SELECT parent_id, grid_id as id, name 
+          FROM {$table['lg']} 
+          WHERE parent_id = {$start_grid_id} 
+          ORDER BY name ASC;
+    " );
+$parent = mysqli_fetch_all($parent_object, MYSQLI_ASSOC);
+$response['list'] = $parent;
+// build full results
+$all_object = mysqli_query( $con, "
+        SELECT parent_id, grid_id as id, name 
+        FROM {$table['lg']};
+    " );
+$all = mysqli_fetch_all($all_object, MYSQLI_ASSOC);
+if ( empty( $all ) ) {
+    _no_results();
+}
+$menu_data = prepare_menu_array( $all );
+$response['text'] = PHP_EOL . 'LOCATION GRID HIERARCHY' . PHP_EOL . PHP_EOL . build_locations_html_list( $start_grid_id, $menu_data, 0, 10 );
+function build_locations_html_list( $parent_id, $menu_data, $gen, $depth_limit ) {
+    $list = '';
+    if ( isset( $menu_data['parents'][$parent_id] ) && $gen < $depth_limit ) {
+        $gen++;
+        foreach ($menu_data['parents'][$parent_id] as $item_id)
+        {
+            switch( $gen ) {
+                case '0':
+                    $list .= '';
+                    break;
+                case '1':
+                    $list .= '';
+                    break;
+                case '2':
+                    $list .= '---- ';
+                    break;
+                case '3':
+                    $list .= '---- ---- ';
+                    break;
+                case '4':
+                    $list .= '---- ---- ---- ';
+                    break;
+                case '5':
+                    $list .= '---- ---- ---- ---- ';
+                    break;
+                case '6':
+                    $list .= '---- ---- ---- ---- ---- ';
+                    break;
+                case '7':
+                    $list .= '---- ---- ---- ---- ---- ---- ';
+                    break;
+            }
+            $list .= $menu_data['items'][ $item_id ]['name'] . ' (' . $item_id . ')' . PHP_EOL;
+            if ( $gen === 1 ) {
+                $list .= PHP_EOL;
+            }
+            $sub = build_locations_html_list( $item_id, $menu_data, $gen, $depth_limit );
+            if ( ! empty( $sub ) ) {
+                $list .= $sub;
+            }
+            if ( $gen === 1 ) {
+                $list .= PHP_EOL . PHP_EOL . '-------------------------------------------------------------------------------------------' . PHP_EOL . PHP_EOL . PHP_EOL;
+            }
+        }
+    }
+    return $list;
+}
+function prepare_menu_array( $query) {
+    // prepare special array with parent-child relations
+    $menu_data = array(
+        'items' => array(),
+        'parents' => array()
+    );
+    foreach ( $query as $menu_item )
+    {
+        $menu_data['items'][$menu_item['id']] = $menu_item;
+        $menu_data['parents'][$menu_item['parent_id']][] = $menu_item['id'];
+    }
+    return $menu_data;
+}
+function _no_results() {
+    return 'No Results';
+}
+
+file_put_contents( $folders['root'] . 'hierarchy.txt', $response['text'] );
+print '#';
+
+
 print '**************************************************************'. PHP_EOL;
 print date('H:i:s') . ' | END SCRIPT'. PHP_EOL;
 print '**************************************************************'. PHP_EOL;
